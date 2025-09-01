f23ed8084ac606124e8bb451675640adbfd722da
Asanka Wanigasekara <awanigasekara83@gmail.com>
2025-09-02 00:57:08 +0100

    Fix remote and prepare for deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import "./App.css";
 // Import all images directly from the src/images folder
 import backgroundImage from "./images/background.jpg";
@@ -8,6 +9,9 @@ import manImage from "./images/man.png";
 import serviceImage1 from "./images/service_img1.jpg";
 import serviceImage2 from "./images/service_img2.jpg";
 import serviceImage3 from "./images/service_img3.jpg";
+=======
+import "./App.css"; // global styles
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
 
 function App() {
   return (
@@ -32,12 +36,20 @@ function App() {
                 <span className="fa fa-clock-o"></span> Mon - Sat 8:00 - 18:00
               </li>
               <li>
+<<<<<<< HEAD
                 <a href="https://facebook.com" target="_blank" rel="noreferrer">
+=======
+                <a href="https://facebook.com">
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
                   <span className="fa fa-facebook"></span>
                 </a>
               </li>
               <li>
+<<<<<<< HEAD
                 <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+=======
+                <a href="https://www.youtube.com">
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
                   <span className="fa fa-youtube"></span>
                 </a>
               </li>
@@ -51,8 +63,12 @@ function App() {
             <a className="navbar-brand" href="#home">
               <img
                 className="logo"
+<<<<<<< HEAD
                 // Use the imported variable for the logo
                 src={logoImage}
+=======
+                src="/images/Logo_itlegendz_bbg.png"
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
                 alt="ITLEGENDZ Logo"
               />
             </a>
@@ -67,6 +83,7 @@ function App() {
       </header>
 
       {/* Hero Section */}
+<<<<<<< HEAD
       <section
         id="home"
         className="home"
@@ -85,6 +102,15 @@ function App() {
       <section className="banner">
         {/* Use the imported variable for the banner image */}
         <img src={bannerImage} alt="Banner" className="banner-img" />
+=======
+      <section id="home" className="home">
+        <div className="overlay"></div>
+      </section>
+
+      {/* Banner */}
+      <section className="banner">
+        <img src="/images/banner.jpg" alt="Banner" className="banner-img" />
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
       </section>
 
       {/* About Section */}
@@ -96,16 +122,25 @@ function App() {
                 <h2>Welcome to Our Company</h2>
                 <h3>Smart Solution Providers</h3>
                 <p>
+<<<<<<< HEAD
                   Founded in June 2025, our company specializes in software
                   development, maintenance, and Quality Assurance engineering
+=======
+                  Founded in June 2025, our company specializes in Software
+                  development, maintenance and Quality Assurance engineering
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
                   services.
                 </p>
               </div>
             </div>
             <div className="col-lg-3 col-md-6 col-lg-offset-1">
               <div className="about_banner">
+<<<<<<< HEAD
                 {/* Use the imported variable for the man image */}
                 <img src={manImage} alt="About Banner" />
+=======
+                <img src="/images/man.png" alt="About Banner" />
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
               </div>
             </div>
           </div>
@@ -118,18 +153,30 @@ function App() {
           <h2>OUR SERVICES</h2>
           <div className="row">
             <div className="col-md-4 service_item">
+<<<<<<< HEAD
               {/* Use the imported variables for service images */}
               <img src={serviceImage1} alt="Service 1" />
+=======
+              <img src="/images/service_img1.jpg" alt="Service 1" />
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
               <h3>Software Development and Maintenance</h3>
               <p>Proven professionals ensuring Quality systems and maintenance.</p>
             </div>
             <div className="col-md-4 service_item">
+<<<<<<< HEAD
               <img src={serviceImage2} alt="Service 2" />
+=======
+              <img src="/images/service_img2.jpg" alt="Service 2" />
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
               <h3>QA Automation</h3>
               <p>Automating testing processes to save time and increase accuracy.</p>
             </div>
             <div className="col-md-4 service_item">
+<<<<<<< HEAD
               <img src={serviceImage3} alt="Service 3" />
+=======
+              <img src="/images/service_img3.jpg" alt="Service 3" />
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
               <h3>Smart App Solutions</h3>
               <p>Tailored smart solutions for both public and private sectors.</p>
             </div>
@@ -145,7 +192,13 @@ function App() {
             <input type="text" placeholder="Name" className="form-control" />
             <input type="email" placeholder="Email" className="form-control" />
             <textarea placeholder="Message" className="form-control"></textarea>
+<<<<<<< HEAD
             <button type="submit" className="btn submit_btn">Submit</button>
+=======
+            <button type="submit" className="btn submit_btn">
+              Submit
+            </button>
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
           </form>
         </div>
       </section>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -6,6 +7,16 @@ import reportWebVitals from "./reportWebVitals";
 
 // Create root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+>>>>>>> 1cfd07c (Fix remote and prepare for deploy)
 
 root.render(
   <React.StrictMode>
