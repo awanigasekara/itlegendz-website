183d342e007384a73a6c136f7de81b5dc3354296
awanigasekara <165375586+awanigasekara@users.noreply.github.com>
2025-09-04 15:52:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 // Create root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
->>>>>>> 1cfd07c (Fix remote and prepare for deploy)
 
 root.render(
   <React.StrictMode>
